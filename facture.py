6886283438936f5ea5d25620ba1bfdac7c6548f0
nathan2612 <nathan.joubert@etu.univ-orleans.fr>
2025-03-21 14:02:46 +0100

    Update facture.py

--- facture.py
+++ facture.py
@@ -1,6 +1,10 @@
+import shutil
 import sqlalchemy
 import argparse
 import getpass
+
+#python3 facture.py --requete ./req_fac.sql --login joubert --serveur servinfo-maria --bd Librairie
+
 
 class MySQL(object):
     def __init__(self, user, passwd, host, database,timeout=20):
@@ -10,7 +14,7 @@ class MySQL(object):
         self.database = database
         #try:
         self.engine = sqlalchemy.create_engine(
-                'mysql+mysqlconnector://' + self.user + ':' + self.passwd + '@' + self.host + '/' + self.database,
+                'mariadb://' + self.user + ':' + self.passwd + '@' + self.host + '/' + self.database,
                 )
         self.cnx = self.engine.connect()
         print("connexion réussie")
@@ -31,14 +35,87 @@ def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
     # et le deuxième ? par l'année
     curseur=bd.execute(requete,(mois,annee))
     # Initialisations du traitement
-    res='pas encore implémentée'
+    print(f"Factures du {mois}/{annee}")
+    taille_terminal = shutil.get_terminal_size().columns
+    res=''
+    livres_vendu = 0
+    commande_prec = None
+    magasin_prec = None
+    ca_global = 0
+    nb_global = 0
     for ligne in curseur:
-        # parcours du résultat de la requête. 
-        # ligne peut être vu comme un dictionnaire dont les clés sont les noms des colonnes de votre requête
-        # est les valeurs sont les valeurs de ces colonnes pour la ligne courante
-        # par exemple ligne['numcom'] va donner le numéro de la commande de la ligne courante 
-        ...
+        nommag = ligne['nommag']
+        numcom = ligne['numcom']
+        datecom = ligne['datecom']
+        nomcli = ligne['nomcli']
+        prenomcli = ligne['prenomcli']
+        adressecli = ligne['adressecli']
+        codepostal = ligne['codepostal']
+        villecli = ligne['villecli']
+        isbn = ligne['isbn']
+        titre = ligne['titre']
+        qte = ligne['qte']
+        prix = ligne['prixvente']
+        Total = ligne['Total']
+        
+        if magasin_prec is None:
+            print(f"Edition des factures du magasin {nommag}")
+            print("-" * taille_terminal)
+            nb_livre = 0
+            facture_editees = 0
+        elif nommag!= magasin_prec:
+            print(str(facture_editees) +" factures éditées")
+            print(str(nb_livre)+" livres vendus")
+            print("*"*taille_terminal)
+            print(f"Edition des factures du magasin {nommag}")
+            print("-" * taille_terminal)
+            nb_livre = 0
+            facture_editees = 0
 
+
+        if commande_prec is None:
+            total = 0
+            print(f"{prenomcli} {nomcli}\n{adressecli}\n{codepostal} {villecli}")
+            print(" " * (taille_terminal//2-15) + "commande n°" + str(numcom) + " du "+str(datecom))
+            print("".ljust(15)+"ISBN".ljust(40)+"Titre".ljust(35)+"qte".ljust(15)+"prix".ljust(10)+"total")
+            print("".ljust(5)+str(nb_livre+1).ljust(5)+str(isbn).ljust(15)+str(titre).ljust(66)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total))
+            total += Total
+            nb_livre += qte
+            facture_editees +=1
+        elif commande_prec!=numcom:
+            print("".ljust(111)+"---------")
+            print("".ljust(110)+"Total "+str(total))
+            print("-"*taille_terminal)
+            ca_global+=total
+            total = 0
+            print(f"{prenomcli} {nomcli}\n{adressecli}\n{codepostal} {villecli}")
+            print(" " * (taille_terminal//2-15) + "commande n°" + str(numcom) + " du "+str(datecom))
+            print("".ljust(15)+"ISBN".ljust(40)+"Titre".ljust(35)+"qte".ljust(15)+"prix".ljust(10)+"total")
+            print("".ljust(5)+str(nb_livre).ljust(5)+str(isbn).ljust(15)+str(titre).ljust(66)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total))
+            total += Total
+            nb_livre += qte
+            facture_editees +=1
+        else:
+            print("".ljust(5)+str(nb_livre).ljust(5)+str(isbn).ljust(15)+str(titre).ljust(66)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total))
+            total += Total
+            nb_livre += qte
+            facture_editees +=1
+            
+        magasin_prec = nommag
+        commande_prec = numcom
+    print("".ljust(111)+"---------")
+    print("".ljust(110)+"Total "+str(total))
+    print("-"*taille_terminal)
+    print(str(facture_editees) +" factures éditées")
+    print(str(nb_livre)+" livres vendus")
+    print("*"*taille_terminal)
+    print(f"Edition des factures du magasin {nommag}")
+    print("-" * taille_terminal)
+    print("Chiffre d'affaire global : "+ca_global)
+    print("Nombre livres vendus "+nb_global)
+
+
+        
     #ici fin du traitement
     # fermeture de la requête
     curseur.close()
