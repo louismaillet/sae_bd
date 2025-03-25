e57e4cabb700ddbe813a4bed0528ab54cfa257e3
nathan2612 <nathan.joubert@etu.univ-orleans.fr>
2025-03-25 14:38:13 +0100

    Update facture.py

--- facture.py
+++ facture.py
@@ -33,17 +33,16 @@ def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
     # exécute la requête en remplaçant le premier ? par le numéro du mois 
     # et le deuxième ? par l'année
     curseur=bd.execute(requete,(mois,annee))
-    # Initialisations du traitement
-    print(f"Factures du {mois}/{annee}")
-    requetes = list(curseur)
     res=''
-    taille = 115
+    # Initialisations du traitement
+    res += f"Factures du {mois}/{annee}\n"
+    taille_terminal = 115
+    commande_prec = None
+    magasin_prec = None
     ca_global = 0
     nb_global = 0
     nb_commande_livre = 0
-    total = 0
-    for i in range(len(requetes)-1):
-        ligne = requetes[i]
+    for ligne in curseur:
         nommag = ligne['nommag']
         numcom = ligne['numcom']
         datecom = ligne['datecom']
@@ -57,59 +56,79 @@ def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
         qte = ligne['qte']
         prix = ligne['prixvente']
         Total = ligne['Total']
-        if i ==0:
-            res +=f"Facture du {mois}/{annee}"
-            res += f"Edition des factures du magasin {str(nommag)}\n"
-            res += "-"*taille+"\n"   
-            facture_editees = 0
+
+        if magasin_prec is None: #vérification pour le cas du début
+            res += f"Edition des factures du magasin {nommag}\n"
+            res += "-" * taille_terminal + "\n"
             nb_livre = 0
-            nb_commande_livre = 1
-            res += f"{prenomcli} {nomcli}\n{adressecli}\n{codepostal} {villecli}\n"
-            res += f"commande n° {numcom} du {datecom}".center(taille)+"\n"
-            res += "ISBN".rjust(12)+"Titre".rjust(20)+"qte".rjust(56)+"prix".rjust(16)+"total\n".rjust(11)
-        elif numcom != requetes[i+1]['numcom']:
-            res += str(nb_commande_livre).ljust(3)+str(isbn).ljust(15)+str(titre).ljust(68)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total)+"\n"
-            nb_commande_livre = 1
-            total += Total
-            res += "--------\n".rjust(taille)
-            res += ("Total    "+str(total)+"\n").rjust(taille)
-            res +="-"*taille+"\n"
-            ca_global+= total
+            facture_editees = 0
+        elif nommag!= magasin_prec: #si je change de magasin
+            nb_global+=nb_livre
+            ca_global+=total
+            res += "".ljust(111) + "---------\n"
+            res += "".ljust(110) + "Total " + str(total) + "\n"
+            res += "-" * taille_terminal + "\n"
+            res += str(facture_editees) + " factures éditées\n"
+            res += str(nb_livre) + " livres vendus\n"
+            res += "*" * taille_terminal + "\n"
+            print(f"Edition des factures du magasin {nommag}")
+            print("-" * taille_terminal)
+            nb_livre = 0
+            facture_editees = 0
             total = 0
-            nb_livre += qte
-            facture_editees+=1
-            if nommag != requetes[i+1]['nommag']:
-                res += str(facture_editees)+" factures éditées\n"
-                res += str(nb_livre)+" livres vendus\n"
-                res += "*"*taille+"\n"
-                res += "Edition des factures du magasin "+str(nommag)+"\n"
-                res += "-"*taille+"\n"
-                nb_global +=nb_livre   
-                facture_editees = 0
-                nb_livre = 0
-            res += f"{requetes[i+1]['prenomcli']} {requetes[i+1]['nomcli']}\n{requetes[i+1]['adressecli']}\n{requetes[i+1]['codepostal']} {requetes[i+1]['villecli']}\n"
-            res += f"commande n° {requetes[i+1]['numcom']} du {requetes[i+1]['datecom']}".center(taille)+"\n"
-            res += "ISBN".rjust(12)+"Titre".rjust(20)+"qte".rjust(56)+"prix".rjust(16)+"total\n".rjust(11)
-        else:
-            res += str(nb_commande_livre).ljust(3)+str(isbn).ljust(15)+str(titre).ljust(68)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total)+"\n"
-            total += Total
+
+        if commande_prec is None: #vérification pour le cas du début
             nb_commande_livre +=1
-            nb_livre += qte
-        if i==(len(requetes)-2):
-            res += str(nb_commande_livre).ljust(3)+str(isbn).ljust(15)+str(titre).ljust(68)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total)+"\n"
+            res += f"{prenomcli} {nomcli}\n{adressecli}\n{codepostal} {villecli}\n"
+            res += " " * (taille_terminal // 2 - 15) + "commande n°" + str(numcom) + " du " + str(datecom) + "\n"
+            res += "".ljust(15) + "ISBN".ljust(40) + "Titre".ljust(35) + "qte".ljust(15) + "prix".ljust(10) + "total\n"
+            res += "".ljust(5) + str(nb_commande_livre).ljust(5) + str(isbn).ljust(15) + str(titre).ljust(66) + str(qte).ljust(14) + str(prix).ljust(9) + str(Total) + "\n"
+            total = 0
             total += Total
-            ca_global+= total
             nb_livre += qte
-            nb_global +=nb_livre
-            facture_editees+=1
-            res += "--------\n".rjust(taille)
-            res += ("Total    "+str(total)+"\n").rjust(taille)
-            res +="-"*taille+"\n"
-            res += str(facture_editees)+" factures éditées\n"
-            res += str(nb_livre)+" livres vendus\n"
-            res += "*"*taille+"\n"
-            res += f"Chiffre d'affaire global : {ca_global}\n"
-            res += f"Nombre livres vendus : {nb_global}"
+            facture_editees +=1
+
+        elif commande_prec!=numcom: #si je change de commande
+            if nommag == magasin_prec:
+                res += "".ljust(111)+"---------"+"\n"
+                res += "".ljust(110)+"Total "+str(total)+"\n"
+                res += "-"*taille_terminal + "\n"
+            ca_global+=total
+            total = 0
+            nb_commande_livre = 0
+            nb_commande_livre +=1
+
+            res += f"{prenomcli} {nomcli}\n{adressecli}\n{codepostal} {villecli}"+"\n"
+            res += " " * (taille_terminal//2-15) + "commande n°" + str(numcom) + " du "+str(datecom)+"\n"
+            res += "".ljust(15)+"ISBN".ljust(40)+"Titre".ljust(35)+"qte".ljust(15)+"prix".ljust(10)+"total"+"\n"
+            res += "".ljust(5)+str(nb_commande_livre).ljust(5)+str(isbn).ljust(15)+str(titre).ljust(66)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total)+"\n"
+
+            total += Total
+            nb_livre += qte
+            facture_editees +=1
+        else: #si je ne change pas de commande
+            nb_commande_livre +=1
+
+            res +="".ljust(5)+str(nb_commande_livre).ljust(5)+str(isbn).ljust(15)+str(titre).ljust(66)+str(qte).ljust(14)+str(prix).ljust(9)+str(Total)+"\n"
+
+            total += Total
+            nb_livre += qte
+            facture_editees +=1
+
+        magasin_prec = nommag
+        commande_prec = numcom
+
+    nb_global += nb_livre
+    ca_global+=total
+    res += "".ljust(111) + "---------\n"
+    res += "".ljust(110) + "Total " + str(total) + "\n"
+    res += "-" * taille_terminal + "\n"
+    res += str(facture_editees) + " factures éditées\n"
+    res += str(nb_livre) + " livres vendus\n"
+    res += "*" * taille_terminal + "\n"
+    res += "Chiffre d'affaire global : " + str(ca_global) + "\n"
+    res += "Nombre livres vendus " + str(nb_global) + "\n"
+
 
         
     #ici fin du traitement
@@ -126,17 +145,16 @@ if __name__ == '__main__':
     parser.add_argument("--login",dest="nomLogin", help="Nom de login sur le serveur de base de donnée", type=str, default='limet')
     parser.add_argument("--requete", dest="fichierRequete", help="Fichier contenant la requete des commandes", type=str)    
     args = parser.parse_args()
-    #passwd = getpass.getpass("mot de passe SQL:")
-    passwd = "joubert"
+    passwd = 'joubert'
     try:
         ms = MySQL(args.nomLogin, passwd, args.nomServeur, args.nomBaseDeDonnees)
     except Exception as e:
         print("La connection a échoué avec l'erreur suivante:", e)
         exit(0)
-    #rep=input("Entrez le mois et l'année sous la forme mm/aaaa ")
-    #mm,aaaa=rep.split('/')
-    mois=int(2)
-    annee=int(2020)
+    rep='02/2020'
+    mm,aaaa=rep.split('/')
+    mois=int(mm)
+    annee=int(aaaa)
     with open(args.fichierRequete) as fic_req:
         requete=fic_req.read()
     print(faire_factures(requete,mois,annee,ms))
